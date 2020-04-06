55e611fc295ba7693b4516f41e9fa8340a267a38
Ubuntu <ubuntu@ip-172-31-19-33.us-west-2.compute.internal>
2020-04-06 12:51:11 +0000

    Fill .sh and .txt files with solutions

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -5,14 +5,20 @@
 # Step 1:
 # This is your Docker ID/path
 # dockerpath=<>
+dockerpath="bolobolobob/project4api"
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-
+kubectl run project4api\
+    --generator=run-pod/v1\
+    --image=$dockerpath:project4api\
+    --port=80 --labels app=project4api
 
 # Step 3:
 # List kubernetes pods
+kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
+kubectl port-forward project4api 8000:80
 
--- upload_docker.sh
+++ upload_docker.sh
@@ -6,10 +6,14 @@
 # Step 1:
 # Create dockerpath
 # dockerpath=<your docker ID/path>
+dockerpath="bolobolobob/project4api"
 
 # Step 2:  
 # Authenticate & tag
 echo "Docker ID and Image: $dockerpath"
+docker login --username=bolobolobob
+docker tag 862e5d26eabd $dockerpath:project4api
 
 # Step 3:
 # Push image to a docker repository
+docker push $dockerpath
